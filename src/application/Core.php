2a746cd79cd1a2929be52e93638c5c3ba6b940c3
Dominik Skerhak <dominik.skerhak@gmail.com>
2018-09-20 01:19:42 +0200

    Move CRM initialization to application module
    Whole application is now initialized from application module. This was
    moved inside (base) extension so we can change bootstrapping process and
    package users will need to only run composer update. No changes required
    in project files outside of composer package.
    
    Affected files:
    
    - app/bootstrap.php
    - bin/command.php
    
    Both files are now calling methods from Crm/ApplicationModule/Core class.
    
    remp/crm#424

---
+++ src/application/Core.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Crm\ApplicationModule;
+
+use Dotenv\Dotenv;
+use Symfony\Component\Console\Application;
+use Nette\Configurator;
+use Nette\Database\DriverException;
+use Nette\DI\Container;
+use Nette\InvalidArgumentException;
+use Tomaj\Errbit\ErrbitLogger;
+
+class Core
+{
+    /** @var Container */
+    private $container;
+
+    private $environment = null;
+
+    public function __construct($appRootDir = null)
+    {
+        if (!defined('APP_ROOT')) {
+            if (is_null($appRootDir)) {
+                // TODO: [refactoring] try to solve this with some ENV / config variable? or change appRootDir to required?
+                // working with assumption callers will be in placed in default crm-skeleton directories:
+                // - <path-to-project>/bin/command.php
+                // - <path-to-project>/app/bootstrap.php
+                $appRootDir = realpath(dirname($_SERVER["SCRIPT_FILENAME"]) . '/../');
+            }
+
+            define('APP_ROOT', $appRootDir . '/');
+        }
+    }
+
+    public function bootstrap(): Container
+    {
+        $this->init();
+        $this->createContainer();
+        $this->setDatabase();
+        $this->setLogging();
+
+        return $this->container;
+    }
+
+    public function command()
+    {
+        $this->bootstrap();
+        $application = new Application();
+        $application->setCatchExceptions(false);
+
+        new PhinxRegistrator(
+            $application,
+            $this->container->getByType(\Crm\ApplicationModule\EnvironmentConfig::class),
+            $this->container->getByType(\Crm\ApplicationModule\ModuleManager::class)
+        );
+
+        try {
+            /** @var ApplicationManager $applicationManager */
+            $applicationManager = $this->container->getByType(\Crm\ApplicationModule\ApplicationManager::class);
+            $commands = $applicationManager->getCommands();
+            foreach ($commands as $command) {
+                $application->add($command);
+            }
+        } catch (DriverException $driverException) {
+            echo "INFO: Looks like the new fresh install.\n";
+        } catch (InvalidArgumentException $invalidArgument) {
+            echo "INFO: Looks like the new fresh install - or wrong configuration - '{$invalidArgument->getMessage()}'.\n";
+        }
+
+        return $application->run();
+    }
+
+    private function init()
+    {
+        require_once APP_ROOT . 'vendor/autoload.php';
+
+        $envFile = getenv('CRM_ENV_FILE') ?: '.env';
+        $dotenv = new Dotenv(APP_ROOT, $envFile);
+        $dotenv->load();
+        $this->environment = getenv('CRM_ENV');
+        if (!$this->environment) {
+            die('You have to specify environment CRM_ENV');
+        }
+
+        if (getenv('CRM_FORCE_HTTPS') === 'true') {
+            $_SERVER['HTTPS'] = true;
+            $_SERVER['HTTP_X_FORWARDED_PROTO'] = 'https';
+            $_SERVER['SERVER_PORT'] = 443;
+        }
+    }
+
+    private function createContainer()
+    {
+        $configurator = new Configurator;
+
+        // set Nette DIR variables to proper directories (otherwise it leads to application-module path)
+        $configurator->addParameters(array(
+            'appDir' => APP_ROOT . 'app',
+            'wwwDir' => APP_ROOT . 'www',
+        ));
+
+        if ($this->environment == 'local') {
+            $configurator->setDebugMode(true);
+        } else {
+            $configurator->setDebugMode(false);
+        }
+
+        # terminal
+        if (!isset($_SERVER['HTTP_HOST']) && isset($_SERVER['SHELL'])) {
+            $configurator->setDebugMode(true);
+        }
+
+        $configurator->enableDebugger(APP_ROOT . 'log');
+
+        $configurator->setTempDirectory(APP_ROOT . 'temp/nette');
+
+        $configurator->createRobotLoader()
+            ->addDirectory(APP_ROOT . 'app')
+            ->register();
+
+        $configurator->addConfig(APP_ROOT . 'app/config/config.neon');
+        $configurator->addConfig(APP_ROOT . 'app/config/config.' . $this->environment . '.neon');
+
+        $this->container = $configurator->createContainer();
+    }
+
+    private function setDatabase()
+    {
+        $database = $this->container->getByType('Nette\Database\Context');
+        // TODO: [refactoring] test with proper settings 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'
+        $database->query("SET SESSION sql_mode = 'NO_ENGINE_SUBSTITUTION'");
+        $database->query("SET NAMES utf8mb4");
+    }
+
+    private function setLogging()
+    {
+        $errbitConfig = $this->container->parameters['errbit'];
+        ErrbitLogger::register($errbitConfig);
+    }
+}
