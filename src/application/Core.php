2e003b5bf8038d1dcfed2f55a6da801665128416
Miroslav Svítok <mirosvitok@gmail.com>
2018-10-04 12:50:00 +0200

    Export subscribers for Ceska Posta

--- src/application/Core.php
+++ src/application/Core.php
@@ -25,7 +25,8 @@ class Core
                 // working with assumption callers will be in placed in default crm-skeleton directories:
                 // - <path-to-project>/bin/command.php
                 // - <path-to-project>/app/bootstrap.php
-                $appRootDir = realpath(dirname($_SERVER["SCRIPT_FILENAME"]) . '/../');
+                //$appRootDir = realpath(dirname($_SERVER["SCRIPT_FILENAME"]) . '/../');
+                $appRootDir = realpath(__DIR__ . '/../../../../');
             }
 
             define('APP_ROOT', $appRootDir . '/');
