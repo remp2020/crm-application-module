9fcc1ef4f524cf03ac2e4f318fbf058b5e413c3e
Miroslav Svítok <mirosvitok@gmail.com>
2019-11-21 13:54:49 +0100

    Implementing NowTrait useful for testing
    remp/crm#898

---
+++ src/tests/NowTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Crm\ApplicationModule\Tests;
+
+/**
+ * Trait serving as getter and setter of $now value, useful for testing
+ */
+trait NowTrait
+{
+    public function setNow(\DateTime $now)
+    {
+        $this->now = $now;
+    }
+
+    /**
+     * Get new $now value (cloned, to avoid modifying original value)
+     * @return \DateTime
+     * @throws \Exception
+     */
+    public function getNow(): \DateTime
+    {
+        if (!$this->now) {
+            $this->now = new \DateTime('now');
+        }
+
+        return clone $this->now;
+    }
+}
