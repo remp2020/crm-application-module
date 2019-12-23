3b3becd7f10adfd55d999d286f4d2d8145be8e59
Peter Dulacka <dulacka@gmail.com>
2019-12-23 09:50:10 +0100

    Adding new_subscription_delayed event to handle attachments after payment
    Process for generation of invoices in CRM is configured to allow users
    to enter invoice data before we send them the confirmation email. This
    delay allows us to attach them the invoice directly to the confirmation
    email.
    
    As there was no nice solution up to now, we emit new_subscription_delayed
    event when the subscription is created and attempt to generate attachments
    after the configurable delay. Email to Mailer is then sent right away.
    
    remp/crm#890

--- src/model/LogFilter.php
+++ src/model/LogFilter.php
@@ -7,7 +7,7 @@ class LogFilter
     public static function add($filters)
     {
         return function ($record) use ($filters) {
-            foreach ($filters as $filter){
+            foreach ($filters as $filter) {
                 if (isset($record['context']['payload'][$filter])) {
                     $record['context']['payload'][$filter] = '******';
                 }
