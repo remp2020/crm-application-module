dd777467947be0523aa9a7aecb2a6847386753e8
Miroslav Svítok <mirosvitok@gmail.com>
2020-03-03 13:12:07 +0100

    Adding initial schema migration for test, marking test as slow

--- src/commands/DumpSchemaForTests.php
+++ src/commands/DumpSchemaForTests.php
@@ -2,9 +2,11 @@
 
 namespace Crm\ApplicationModule\Commands;
 
-use DirectoryIterator;
 use http\Exception\RuntimeException;
+use Nette\Utils\FileSystem;
+use Nette\Utils\Finder;
 use Odan\Migration\Command\GenerateCommand;
+use Phinx\Util\Util;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -24,16 +26,23 @@ class DumpSchemaForTests extends GenerateCommand
         // GenerateCommand also uses only first migration path
         $migrationPath = $migrationPaths[0];
         // Remove previous initial migration + schema.php
-        foreach (new DirectoryIterator($migrationPath) as $fileInfo) {
-            if(!$fileInfo->isDot()) {
-                unlink($fileInfo->getPathname());
-            }
+        foreach (Finder::findFiles('*.php')->in($migrationPath) as $splFileInfo) {
+            unlink($splFileInfo->getPathname());
         }
 
         // Setting up default options
         $input->setOption('name', 'AllTables');
         $input->setOption('overwrite', 'y');
 
-        return parent::execute($input, $output);
+        $toReturn = parent::execute($input, $output);
+
+        // HACK!
+        // replace migrate file date to fixed date (epoch beginning), so git is enable to track changes
+        $epochBeginning = date(Util::DATE_FORMAT, 0);
+        foreach (Finder::findFiles('*_all_tables.php')->in($migrationPath) as $splFileInfo) {
+            FileSystem::rename($splFileInfo->getPathname(), $splFileInfo->getPath() . DIRECTORY_SEPARATOR . $epochBeginning . '_all_tables.php');
+        }
+
+        return $toReturn;
     }
 }
