10b57708b777093cb7e1d054b59c3a619634e609
Peter Dulacka <dulacka@gmail.com>
2020-03-30 22:55:39 +0200

    Fixing weekscale data rendering due to misuse of ISO-8601 format
    The beginning of the chart was supposed to be a week start of the
    selected scale. Date was being set via ISO-8601 format by setting
    week (correctly acquired by ISO-8601 extraction) and year
    (incorrectly acquired by using default `Y` format instead of
    ISO-8601-based `o` format which considers the week shift.
    
    An example:
    
        $date = new \DateTime('2019-12-30');
        // formatted to `Y` - `2019`, `W` - `01`, `o` - `2020`
    
        $date->setISODate(2019, 01) // '2018-12-31'
        $date->setIsoDate(2020, 01) // '2019-12-30'
    
    remp/crm#1131

--- src/model/Graphs/Scale/WeekScale.php
+++ src/model/Graphs/Scale/WeekScale.php
@@ -12,10 +12,10 @@ abstract class WeekScale extends ScaleBase implements ScaleInterface
         $endDateTime = new DateTime(date('Y-m-d', strtotime($end)));
         $diff = $actual->diff($endDateTime);
 
-        $days = intval($diff->format('%a'));
+        $days = (int)$diff->format('%a');
         $weeks = ceil($days / 7);
         $result = [];
-        $actual->setISODate($actual->format('Y'), $actual->format('W'));
+        $actual = $actual->setISODate($actual->format('o'), $actual->format('W'));
         $result[$actual->format('Y-m-d')] = "new Date({$actual->format('Y,n-1,j')})";
         for ($i = 0; $i < $weeks; $i++) {
             $actual = $actual->modify('+1 week');
