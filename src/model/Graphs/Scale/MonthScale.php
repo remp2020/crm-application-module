2e01de850a3939708528a86e24cfb25d2831d869
Peter Dulacka <dulacka@gmail.com>
2020-03-30 16:50:07 +0200

    Fixing month scale chart issues with missing February data
    remp/crm#1125

--- src/model/Graphs/Scale/MonthScale.php
+++ src/model/Graphs/Scale/MonthScale.php
@@ -12,12 +12,12 @@ abstract class MonthScale extends ScaleBase implements ScaleInterface
         $endDateTime = new DateTime(date('Y-m-d', strtotime($end)));
         $diff = $actual->diff($endDateTime);
 
-        $months = intval($diff->m + $diff->y * 12);
+        $months = (int) $diff->m + $diff->y * 12;
         $result = [];
 
         $result[$actual->format('Y-n')] = "new Date({$actual->format('Y,n-1')})";
         for ($i = 0; $i < $months; $i++) {
-            $actual = $actual->modify('+1 month');
+            $actual = $actual->modify('first day of next month');
             $result[$actual->format('Y-n')] = "new Date({$actual->format('Y,n-1')})";
         }
 
