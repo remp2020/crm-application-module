28a15e05bf733cf647969664f231dc62ad605b74
Lubos Michalik <lubos.michalik@projektn.sk>
2020-08-11 13:50:52 +0200

    Added the scenario's condition criteria `IsActiveRecurrentSubscriptionCriteria` to check the existence of active recurrent subscription for the payment.
     remp/crm#1286
    
    Change the signature of method `addCondition` of `ScenariosCriteriaInterface`. Removed obsolete parameter `key`. Added parameter `criterionItemRow` to allow evaluation based on the event trigger and added return type `bool`.
    
    remp/crm#1268
    
    After rebase changes.
    
    remp/crm#1268
    
    Add doc block for `addCondition` method of `ScenariosCriteriaInterface`.
    
    remp/crm#1268
    
    Update PHP Doc for `addCondition` function of `ScenarioCriteriaInterface`.
    
    remp/crm#1268

--- src/model/Criteria/ScenariosCriteriaInterface.php
+++ src/model/Criteria/ScenariosCriteriaInterface.php
@@ -2,6 +2,7 @@
 
 namespace Crm\ApplicationModule\Criteria;
 
+use Nette\Database\Table\IRow;
 use Nette\Database\Table\Selection;
 
 interface ScenariosCriteriaInterface
@@ -12,12 +13,29 @@ interface ScenariosCriteriaInterface
      * The generator and UI currently support following type of parameters:
      *
      *   * StringLabeledArrayParam
+     *   * BooleanParam
      *
      * @return CriteriaParam[]
      */
     public function params(): array;
 
-    public function addCondition(Selection $selection, $key, $values);
+    /**
+     * Adds condition to $selection query according to $values parameter.
+     * Returns false if criteria based on $criterionItemRow is evaluated as untrue otherwise returns true.
+     *
+     * @param Selection $selection
+     * @param $values - is object containing data returned by user interface settings (from Scenario Builder)
+     *  of assigned CriteriaParam (one you return in params() method). For example, BooleanParam may return object
+     *  {"selection": true}, specifying that condition parameter should be evaluated as true.
+     *  One should reflect such settings in condition added to $selection query.
+     * @param IRow $criterionItemRow - contains IRow object on which $selection query is going to be built.
+     *  This depends on which event you have registered the criteria (see CrmModule#registerScenariosCriteria()).
+     *  For example, if the criteria is registered on subscription event, the criteria will receive the subscription
+     *  object that has triggered the particular scenario.
+     *
+     * @return bool
+     */
+    public function addCondition(Selection $selection, $values, IRow $criterionItemRow): bool;
 
     /**
      * label returns human-friendly and descriptive label of the whole Criteria
