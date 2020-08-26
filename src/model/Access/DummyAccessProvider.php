fbc2b674acd0d239a3c0c24ac310702cfa4587b9
Adam Zoldak <zoldakadam@gmail.com>
2020-08-26 08:21:41 +0200

    Manipulate with device token in TokenStorage
    Device token is now added/removed in TokenStorage based on the link
    between device token and access tokens.
    
    Adding new events for external modules to participate on this
    manipulation:
    
    - `PairDeviceAccessTokensEvent`
    - `UnpairDeviceAccessTokensEvent`
    - `BeforeRemoveAccessTokenEvent`
    
    Handling is covered by tests.
    
    Adding possibility to specify `DummyAccessProvider` rules from within
    the tests to control access behavior for different users.
    
    Co-Authored-By: Peter Dulacka <dulacka@gmail.com>
    
    remp/crm#1312
    
    Add tests, fix access token removal-related issues
    
    remp/crm#1312
    
    Adding additional test scenarios, fixing event-based issues
    
    - Soft device token unpair was not handled the same way
    as access token removal. New event `UnpairDeviceAccessTokenEvent`
    is now emitted to handle this scenario.
    - `PairDeviceAccessTokensEvent` was emitted to soon and
    colided with unpair events. Now it's being trigerred right
    before the pairing.
    - More explanatory name of methods and variables
    in `RemovedAccessTokenEvent`.
    
    remp/crm#1421

--- src/model/Access/DummyAccessProvider.php
+++ src/model/Access/DummyAccessProvider.php
@@ -4,8 +4,35 @@ namespace Crm\ApplicationModule\Access;
 
 class DummyAccessProvider implements ProviderInterface
 {
+    private $rules;
+
+    /**
+     * @param array $rules
+     *
+     * Array with configuration which "access" type should be allowed and which should not.
+     * Rules can be configured statically or via callables:
+     *
+     *  [
+     *      'web' => true,
+     *      'foo' => function ($userId) {
+     *          if ($userId == 1) return true;
+     *          return false;
+     *      }
+     *  ]
+     */
+    public function configure(array $rules)
+    {
+        $this->rules = $rules;
+    }
+
     public function hasAccess($userId, $access)
     {
+        if (isset($this->rules[$access])) {
+            if (is_callable($this->rules[$access])) {
+                return $this->rules[$access]($userId);
+            }
+            return $this->rules[$access];
+        }
         return true;
     }
 
