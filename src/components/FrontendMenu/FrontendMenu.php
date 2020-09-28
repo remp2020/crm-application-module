88258a6d80a44fcf77aec7923c0b0192a42f3026
Adam Zoldak <adam.zoldak@projektn.sk>
2020-09-28 12:22:36 +0000

    Add site URL to frontend menu links
    Bug: In case frontend menu is rendered in ProductsModule (shop), frontend menu links are invalid.
    
    Using  from application config to always route frontend links to main domain.
    
    remp/novydenik#633

--- src/components/FrontendMenu/FrontendMenu.php
+++ src/components/FrontendMenu/FrontendMenu.php
@@ -2,6 +2,7 @@
 
 namespace Crm\ApplicationModule\Components;
 
+use Crm\ApplicationModule\Config\ApplicationConfig;
 use Crm\ApplicationModule\Menu\MenuContainerInterface;
 use Nette\Application\UI;
 
@@ -19,6 +20,15 @@ class FrontendMenu extends UI\Control
     /** @var MenuContainerInterface */
     private $menuItems;
 
+    public $applicationConfig;
+
+    public function __construct(
+        ApplicationConfig $applicationConfig
+    ) {
+        parent::__construct();
+        $this->applicationConfig  = $applicationConfig;
+    }
+
     public function setMenuItems(MenuContainerInterface $menuItems)
     {
         $this->menuItems = $menuItems;
@@ -31,6 +41,7 @@ class FrontendMenu extends UI\Control
 
     public function render()
     {
+        $this->template->siteUrl = $this->applicationConfig->get('site_url');
         $this->template->menuItems = $this->menuItems->getMenuItems();
         $this->template->setFile(__DIR__ . '/' . $this->templateName);
         $this->template->render();
