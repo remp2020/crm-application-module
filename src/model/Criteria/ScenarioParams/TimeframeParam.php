df33824476b089f2c8081fc15cb4305e1f363378
Dominik Skerhak <dominik.skerhak@gmail.com>
2021-03-25 10:59:42 +0100

    Add TimeframeParam, use it in OnboardingGoalCompletedCriteria
    - Added new type of parameter for scenario criteria handling operator,
      value and unit.
    - Used it in `OnboardingGoalCompletedCriteria` where it handles check
      of user's completed goals within timeframe set in scenario's condition
      node.
    
    With this new param, scenario can contain condition which checks not
    only if user completed goal, but it can also check (eg.) if user completed
    this goal within last year. This condition can be used together with
    goal node to filter our users who completed goal in past.
    
    remp/crm#1764

---
+++ src/model/Criteria/ScenarioParams/TimeframeParam.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Crm\ApplicationModule\Criteria\ScenarioParams;
+
+use Crm\ApplicationModule\Scenarios\ScenarioCriteriaParamInterface;
+
+class TimeframeParam implements ScenarioCriteriaParamInterface
+{
+    protected $type = 'timeframe';
+
+    /** @var string */
+    private $key;
+
+    /** @var string */
+    private $label;
+
+    /** @var string */
+    private $amountLabel;
+
+    /** @var string */
+    private $unitsLabel;
+
+    /** @var array */
+    private $operators;
+
+    /** @var array */
+    private $units;
+
+    /** @var array */
+    private $amountInputAttributes;
+
+    /**
+     * DateTimeParam constructor.
+     *
+     * @param string    $key                        ID of the param
+     * @param string    $label                      text label of the parameter
+     * @param string    $amountLabel                text shown shown as a label of the amount input
+     * @param string    $unitsLabel                 text shown shown as a label of the unit input
+     * @param array     $operators                  list of operators to select from; selected operator will be used to compare the number input with
+     * @param array     $units                      list of timeframe units to select from; selected unit will be used with amount (eg. hours, days, months)
+     * @param array     $amountInputAttributes      attributes passed down to <input> HTML tag (e.g. ['min' => 1, 'max' => 10, 'step' => 2]
+     */
+    public function __construct(
+        string $key,
+        string $label,
+        string $amountLabel = 'Amount',
+        string $unitsLabel = 'Units',
+        array $operators = ['=', '>', '<', '>=', '<='],
+        array $units = ['days', 'months', 'years'],
+        array $amountInputAttributes = ['min' => 0]
+    ) {
+        $this->key = $key;
+        $this->label = $label;
+        $this->amountLabel = $amountLabel;
+        $this->unitsLabel = $unitsLabel;
+        $this->operators = $operators;
+        $this->units = $units;
+        $this->amountInputAttributes = $amountInputAttributes;
+    }
+
+    public function key(): string
+    {
+        return $this->key;
+    }
+
+    public function label(): string
+    {
+        return $this->label;
+    }
+
+    public function blueprint(): array
+    {
+        $result = array_filter([
+            'key' => $this->key(),
+            'type' => $this->type(),
+            'label' => $this->label(),
+            'amountLabel' => $this->amountLabel,
+            'unitsLabel' => $this->unitsLabel,
+            'operators' => $this->operators,
+            'units' => $this->units,
+            'amountInputAttributes' => $this->amountInputAttributes,
+        ]);
+        return $result;
+    }
+
+    public function type(): string
+    {
+        return $this->type;
+    }
+}
