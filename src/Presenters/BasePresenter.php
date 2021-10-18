546fc99ded6858fe09f78f9664f6ec0781402d83
Peter Dulacka <dulacka@gmail.com>
2021-10-18 15:17:11 +0200

    Fix missing homeRoute in action and signal presenter methods

--- src/Presenters/BasePresenter.php
+++ src/Presenters/BasePresenter.php
@@ -58,12 +58,13 @@ abstract class BasePresenter extends Presenter
                 $this->redirect('this');
             }
         }
+
+        $this->homeRoute = ':' . $this->applicationConfig->get('home_route');
     }
 
     protected function beforeRender()
     {
         $this->locale = $this->translator->getLocale();
-        $this->homeRoute = ':' . $this->applicationConfig->get('home_route');
         $this->template->homeRoute = $this->homeRoute;
         $this->template->ENV = Core::env('CRM_ENV');
         $this->template->locale = $this->locale;
