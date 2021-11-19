7a3a765e80762597a55fa89c701a8ed119d9bf11
Dominik Skerhak <dominik.skerhak@gmail.com>
2021-11-19 10:52:52 +0100

    Move table cleanup from tearDown() to setUp()
    It is better to cleanup database before test is run. Each test can say
    through `requiredRepositories()` which repositories it needs. And these
    tables will be emptied before test is run. No need to rely on previously
    run tests that they cleaned up all tables.
    
    remp/crm#2111

--- src/tests/DatabaseTestCase.php
+++ src/tests/DatabaseTestCase.php
@@ -5,7 +5,6 @@ namespace Crm\ApplicationModule\Tests;
 use Crm\ApplicationModule\Seeders\ISeeder;
 use Nette\DI\Container;
 use Nette\Database\Context;
-use PDOException;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -49,16 +48,7 @@ abstract class DatabaseTestCase extends TestCase
         foreach ($this->requiredRepositories() as $repositoryClass) {
             $this->repositories[$repositoryClass] = $this->inject($repositoryClass);
         }
-        /** @var  $seederClass */
-        foreach ($this->requiredSeeders() as $seederClass) {
-            /** @var ISeeder $seeder */
-            $seeder = $this->inject($seederClass);
-            $seeder->seed($this->inject(OutputInterface::class));
-        }
-    }
 
-    protected function tearDown(): void
-    {
         $truncateTables = implode(' ', array_map(function ($repo) {
             $property = (new \ReflectionClass($repo))->getProperty('tableName');
             $property->setAccessible(true);
@@ -71,11 +61,13 @@ SET FOREIGN_KEY_CHECKS=0;
 {$truncateTables}
 SET FOREIGN_KEY_CHECKS=1;
 ";
+        $db->exec($sql);
 
-        try {
-            $db->exec($sql);
-        } catch (PDOException $e) {
-            echo $e->getMessage();
+        /** @var  $seederClass */
+        foreach ($this->requiredSeeders() as $seederClass) {
+            /** @var ISeeder $seeder */
+            $seeder = $this->inject($seederClass);
+            $seeder->seed($this->inject(OutputInterface::class));
         }
     }
 
