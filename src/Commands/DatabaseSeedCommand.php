bdedac664ea8caa9fb08b2558e36e93080d0611e
Dominik Skerhak <dominik.skerhak@gmail.com>
2022-06-07 21:03:54 +0200

    Add option to run one only selected seeder
    Command `applicaton:seed` runs all registered seeders. Using new option
    `--seeder` will allow to run only this one selected seeder (if found).
    
    Seeder has to be registered with SeederManager (as usual).
    
    Usage:
    
    ```
    php bin/command.php application:seed --seeder=Crm\\ApplicationModule\\Seeders\\ConfigsSeeder
    
    php bin/command.php application:seed --seeder="Crm\ApplicationModule\Seeders\ConfigsSeeder"
    
    php bin/command.php application:seed -s "Crm\ApplicationModule\Seeders\ConfigsSeeder"
    ```
    
    remp/crm#2477

--- src/Commands/DatabaseSeedCommand.php
+++ src/Commands/DatabaseSeedCommand.php
@@ -5,6 +5,7 @@ namespace Crm\ApplicationModule\Commands;
 use Crm\ApplicationModule\SeederManager;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DatabaseSeedCommand extends Command
@@ -17,27 +18,46 @@ class DatabaseSeedCommand extends Command
         $this->seederManager = $seederManager;
     }
 
-    /**
-     * Configure command
-     */
     protected function configure()
     {
         $this->setName('application:seed')
-            ->setDescription('Seed database with required values');
+            ->setDescription('Seed database with required values')
+            ->addOption(
+                'seeder',
+                's',
+                InputOption::VALUE_REQUIRED,
+                'Single seeder to run. Seeder has to be registered. Whole namespace is required, see usage.',
+                null
+            )
+            ->addUsage('--seeder=Crm\\ApplicationModule\\Seeders\\ConfigsSeeder (escaped backslashes)')
+            ->addUsage('--seeder="Crm\ApplicationModule\Seeders\ConfigsSeeder" (quoted string)')
+            ->addUsage('-s "Crm\ApplicationModule\Seeders\ConfigsSeeder" (short option, quoted string')
+        ;
     }
 
-    /**
-     * @param \Symfony\Component\Console\Input\InputInterface $input
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('');
         $output->writeln('<info>***** SEED DATABASE *****</info>');
         $output->writeln('');
 
-        foreach ($this->seederManager->getSeeders() as $seeder) {
+        $selectedSeeder = $input->getOption('seeder');
+        if ($selectedSeeder === null) {
+            // no seeder selected; get all registered seeders
+            $seeders = $this->seederManager->getSeeders();
+        } else {
+            $seeders = array_filter($this->seederManager->getSeeders(), function ($seeder) use ($selectedSeeder) {
+                return get_class($seeder) === $selectedSeeder;
+            });
+            if (count($seeders) !== 1) {
+                $output->writeln("<error>Selected seeder <info> {$selectedSeeder} </info> was not found.</error>");
+                return Command::FAILURE;
+            }
+        }
+
+        foreach ($seeders as $seeder) {
             $className = get_class($seeder);
+
             $output->writeln("Seeding <info>{$className}</info>");
             $seeder->seed($output);
         }
