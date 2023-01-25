15425dc6e78719be51c872939159d251e4086f32
Peter Dulacka <dulacka@gmail.com>
2023-01-25 07:50:22 +0100

    Fix discrepancies in localized date formatting
    We need to match against one of the two possible values, because of the
    change in the ICU system library 72.1.
    
    Since it's not possible to determine current version, and there might or
    might not be a non-breaking space between the time and AM/PM, we just
    test this against one of the two values.
    
    Official php-fpm docker image uses ICU v67.1, but the php-apline uses
    72.1 and this behaved unexpectedly.

--- src/Tests/LocalizedDateHelperTest.php
+++ src/Tests/LocalizedDateHelperTest.php
@@ -27,7 +27,22 @@ class LocalizedDateHelperTest extends CrmTestCase
         $this->translator->setLocale($locale);
 
         // no locale format set, use default
-        $this->assertEquals('6/28/22, 10:52:15 AM', $this->localizedDateHelper->process($date));
+
+        // We need to match against one of the two possible values, because of the change in the ICU system library 72.1.
+        // Since it's not possible to determine current version, and there might or might not be a non-breaking space
+        // between the time and AM/PM, we just test this against one of the two values.
+        //
+        // Official php-fpm docker image uses ICU v67.1, but the php-apline uses 72.1 and this behaved unexpectedly.
+        // This should be save to remove in 2024.
+        //
+        // https://github.com/php/php-src/issues/10262
+        $this->assertContains(
+            $this->localizedDateHelper->process($date),
+            [
+                '6/28/22, 10:52:15 AM',
+                '6/28/22, 10:52:15 AM'
+            ]
+        );
 
         // locale format set
         $this->localizedDateHelper->setShortFormat($localeFormat, $locale);
@@ -42,7 +57,14 @@ class LocalizedDateHelperTest extends CrmTestCase
         $this->translator->setLocale($locale);
 
         // no locale format set, use default
-        $this->assertEquals('June 28, 2022 at 10:52:15 AM', $this->localizedDateHelper->process($date, true));
+
+        $this->assertContains(
+            $this->localizedDateHelper->process($date, true),
+            [
+                'June 28, 2022 at 10:52:15 AM',
+                'June 28, 2022 at 10:52:15 AM'
+            ]
+        );
 
         // locale format set
         $this->localizedDateHelper->setLongFormat($localeFormat, $locale);
