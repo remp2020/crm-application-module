d011326573389e3affaf2c8a680079ecefa2c9e5
Peter Dulacka <dulacka@gmail.com>
2023-12-13 13:55:39 +0100

    Update league flysystem to v3
    MountManager now doesn't expose previously used public
    methods and the configuration needs to be provided
    in constructor and not later.
    
    MountManagerConfig service is added to manage these
    changes.
    
    Commit also handles changes in the Flysystem classes.
    
    remp/crm#2835

--- src/Models/ApplicationMountManager.php
+++ src/Models/ApplicationMountManager.php
@@ -2,33 +2,50 @@
 
 namespace Crm\ApplicationModule\Models;
 
-use League\Flysystem\FilesystemInterface;
+use League\Flysystem\FilesystemException;
 use League\Flysystem\MountManager;
+use League\MimeTypeDetection\FinfoMimeTypeDetector;
 
 class ApplicationMountManager extends MountManager
 {
-    private $groups = [];
+    private const BUCKET_DELIMITER = '://';
 
-    public function __construct(array $filesystems = [])
+    private array $groups = [];
+
+    private FinfoMimeTypeDetector $mimeTypeDetector;
+
+    public function __construct(protected MountManagerConfig $mountManagerConfig)
     {
+        $filesystems = [];
+        foreach ($mountManagerConfig->getFilesystems() as $key => $filesystem) {
+            $this->groups[$filesystem['group'] ?? null][] = $key;
+            $filesystems[$key] = $filesystem['filesystem'];
+        }
+
+        $this->mimeTypeDetector = new FinfoMimeTypeDetector();
+
         parent::__construct($filesystems);
-    }
-
-    public function mountFilesystem($prefix, FilesystemInterface $filesystem, ?string $group = null)
-    {
-        $this->groups[$group][] = $prefix;
-        return parent::mountFilesystem($prefix, $filesystem); //
     }
 
     public function getFilePath($bucket, $filename): string
     {
-        return $bucket . '://' . $filename;
+        return $bucket . self::BUCKET_DELIMITER . $filename;
     }
 
-    public function getListContents($bucket, ?int $sort = SORT_DESC, ?string $sortColumn = 'timestamp')
+    public function getFileName(string $path): string
     {
-        $path = $bucket . '://';
-        $files = $this->listContents($path);
+        $filePath = explode(self::BUCKET_DELIMITER, $path);
+
+        return end($filePath);
+    }
+
+    /**
+     * @throws FilesystemException
+     */
+    public function getListContents($bucket, ?int $sort = SORT_DESC, ?string $sortColumn = 'lastModified'): array
+    {
+        $path = $bucket . self::BUCKET_DELIMITER;
+        $files = $this->listContents($path)->toArray();
 
         if (isset($sort)) {
             $files = $this->sortFiles($files, $sort, $sortColumn);
@@ -37,9 +54,17 @@ class ApplicationMountManager extends MountManager
         return $files;
     }
 
-    public function getContentsForGroup($group, ?int $sort = SORT_DESC, ?string $sortColumn = 'timestamp')
+    /**
+     * @throws FilesystemException
+     */
+    public function getContentsForGroup($group, ?int $sort = SORT_DESC, ?string $sortColumn = 'lastModified'): array
     {
         $files = [];
+
+        if (!array_key_exists($group, $this->groups)) {
+            return $files;
+        }
+
         foreach ($this->groups[$group] as $bucket) {
             $files[] = $this->getListContents($bucket, null);
         }
@@ -52,12 +77,12 @@ class ApplicationMountManager extends MountManager
         return $files;
     }
 
-    public function getBucketsForGroup($group)
+    public function getBucketsForGroup($group): array
     {
         return $this->groups[$group] ?? [];
     }
 
-    private function sortFiles($files, $sort, $column)
+    private function sortFiles($files, $sort, $column): array
     {
         $columnData = [];
         foreach ($files as $key => $row) {
@@ -66,5 +91,10 @@ class ApplicationMountManager extends MountManager
         array_multisort($columnData, $sort, $files);
 
         return $files;
+    }
+
+    public function getMimetype($path): string
+    {
+        return $this->mimeTypeDetector->detectMimeTypeFromPath($path);
     }
 }
