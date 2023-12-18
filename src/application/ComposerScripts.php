4c5ef78d851a8ad70e0cf619c56aa144f99d9cd5
Peter Dulacka <dulacka@gmail.com>
2023-12-18 10:06:10 +0100

    Replace post autoload dump script with command
    There is an issue caused by upgrade to psr/log 3.*
    which changed its interfaces and added extra types.
    The composer/composer library used by ComposerScripts
    has its own logger interface which is not compatible
    with psr/log 3.*.
    
    If composer's interface gets loaded and then any
    psr/log 3.* gets loaded, it causes this error:
    
      ERROR: Declaration of Psr\Log\AbstractLogger::emergency(Stringable|string $message, array $context = []): void must be compatible with Psr\Log\LoggerInterface::emergency($message, array $context = [])
    
    In our case it was the Sentry library that triggered
    this error by initializing NullLogger, but in general
    it could have been any class that was being instantiated
    while executing any of the post autoload dump commands.
    
    Since there isn't a nice solution how to avoid this and
    Composer isn't going to update the interfaces any time
    soon (see https://github.com/composer/composer/issues/11246),
    we cannot try to initialize Console application at this
    point and we just simply need to execute the command
    in a separate process.
    
    remp/crm#3050

--- src/application/ComposerScripts.php
+++ src/application/ComposerScripts.php
@@ -8,65 +8,23 @@ use Composer\Script\Event;
 use Composer\SelfUpdate\Versions;
 use Nette\Database\DriverException;
 use Nette\InvalidArgumentException;
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Input\InputInterface;
 
 class ComposerScripts
 {
-    /**
-     * Handle the post-autoload-dump Composer event.
-     *
-     * @param  \Composer\Script\Event  $event
-     * @return void
-     */
-    public static function postAutoloadDump(Event $event)
+    public static function postAutoloadDump(Event $event): void
     {
         $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
 
-        // This is initialized before calling require_once. Autoloader would otherwise pick up some different (internal)
-        // version of Symfony dependencies and cause compatibility issues.
-        $installAssets = new ArrayInput(['command' => 'application:install_assets']);
-
-        require_once $vendorDir . '/autoload.php';
-
         if (file_exists($vendorDir . '/../.env')) {
             try {
-                self::runCommand($event, $installAssets);
+                system('php bin/command.php application:install_assets'); // @phpstan-ignore-line
             } catch (DriverException | InvalidArgumentException $exception) {
                 $event->getIO()->write("<warning> CRM </warning> Unable to run <comment>application:install_assets</comment> command, please run <comment>php bin/command.php phinx:migrate</comment> command first.");
             }
         }
-
-        // Running ComposerScripts via Composer (e.g. via post-dump-autoload hook) may pass
-        // different parameters to Nette container builder than when it runs in a regular PHP script.
-        // Nette container builder may not discover all presenters correctly and container may not be initialized properly (see 'scanComposer' and 'scanDirs' in ApplicationExtension for details).
-        // This can cause problems in commands working with presenters - for example, when registering user sources, some presenters could be skipped.
-        // Solution: touch a random file (here we've chosen config.neon), so container is forced to reload in a subsequent command/script.
-        touch($vendorDir . '/../app/config/config.neon');
     }
 
-    private static function runCommand(Event $event, InputInterface $input)
-    {
-        $core = new \Crm\ApplicationModule\Core(
-            realpath($event->getComposer()->getConfig()->get('vendor-dir') . '/../')
-        );
-        $container = $core->bootstrap();
-        $application = new Application();
-        $application->setAutoExit(false);
-        $application->setCatchExceptions(false);
-
-        /** @var ApplicationManager $applicationManager */
-        $applicationManager = $container->getByType(\Crm\ApplicationModule\ApplicationManager::class);
-        $commands = $applicationManager->getCommands();
-        foreach ($commands as $command) {
-            $application->add($command);
-        }
-
-        $application->run($input);
-    }
-
-    public static function checkVersion(Event $event)
+    public static function checkVersion(Event $event): void
     {
         $currentVersion = Composer::getVersion();
 
