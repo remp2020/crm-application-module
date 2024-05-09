25aab879a892043c59d9052cf3b994a3caefd027
Peter Burian <peter.burian@dennikn.sk>
2024-05-09 07:43:17 +0000

    ConditionCheckEventHandler Refactor - inlined condition models moved to separated and tested classes
    remp/crm#1409

--- src/Models/Criteria/ScenarioConditionModelInterface.php
+++ src/Models/Criteria/ScenarioConditionModelInterface.php
@@ -10,7 +10,7 @@ interface ScenarioConditionModelInterface
      * This method should return database Selection with filtered row
      * which will be used as base selection for applying scenario conditions.
      *
-     * @param $scenarioJobParameters array of scenario job parameters fetched from scenarios_jobs->parameters database column
+     * @param object $scenarioJobParameters object of scenario job parameters fetched from scenarios_jobs->parameters database column
      * @return Selection
      */
     public function getItemQuery($scenarioJobParameters): Selection;
