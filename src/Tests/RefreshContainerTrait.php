ed62826335c58509f76657c6cd06f38d920afa40
Peter Dulacka <dulacka@gmail.com>
2024-11-15 10:14:59 +0100

    Fix connection-related issues in tests
    Some tests called refreshContainer duplicitly
    which caused the first connection to be abandoned
    and not disconnected ever.
    
    These connections piled up and caused Too many
    connections errors in the CI because no new connections
    could be created.

--- src/Tests/RefreshContainerTrait.php
+++ src/Tests/RefreshContainerTrait.php
@@ -3,11 +3,17 @@
 namespace Crm\ApplicationModule\Tests;
 
 use Nette\Bootstrap\Configurator;
+use Nette\Database\Explorer;
 
 trait RefreshContainerTrait
 {
     public function refreshContainer()
     {
+        if ($this->container) {
+            /** @var Explorer $database */
+            $database = $this->container->getByType(Explorer::class);
+            $database->getConnection()->disconnect();
+        }
         /** @var Configurator $configurator */
         $configurator = $GLOBALS['configurator'];
         $GLOBALS['container'] = $configurator->createContainer();
