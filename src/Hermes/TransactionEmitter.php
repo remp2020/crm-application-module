5ed82be477955038eb92b6dd5a4cd8ef4d41d8e6
Peter Burian <peter.burian@dennikn.sk>
2024-11-21 13:50:42 +0000

    TransactionEmitter - not initialized error fix
    remp/crm#2218

--- src/Hermes/TransactionEmitter.php
+++ src/Hermes/TransactionEmitter.php
@@ -10,7 +10,7 @@ use Tomaj\Hermes\EmitterInterface;
 
 class TransactionEmitter extends Emitter
 {
-    private array $delayedMessages;
+    private array $delayedMessages = [];
 
     public function __construct(
         private readonly DatabaseTransaction $databaseTransaction,
