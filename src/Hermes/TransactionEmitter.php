a358b80f9746f0af75b6c69c8a918ac2d7a385bf
Peter Burian <peter.burian@dennikn.sk>
2024-11-21 12:25:47 +0000

    Added new extended implementation of Hermes Emitter which supports delaying event emitting until the transaction commit or throwing events emitted during the transaction away if the transaction was rolled back.
    remp/crm#2218
    + related remp/crm#3330

---
+++ src/Hermes/TransactionEmitter.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace Crm\ApplicationModule\Hermes;
+
+use Crm\ApplicationModule\Database\DatabaseTransaction;
+use Psr\Log\LoggerInterface;
+use Tomaj\Hermes\Driver\DriverInterface;
+use Tomaj\Hermes\Emitter;
+use Tomaj\Hermes\EmitterInterface;
+
+class TransactionEmitter extends Emitter
+{
+    private array $delayedMessages;
+
+    public function __construct(
+        private readonly DatabaseTransaction $databaseTransaction,
+        DriverInterface $driver,
+        LoggerInterface $logger = null,
+    ) {
+        parent::__construct($driver, $logger);
+
+        $this->databaseTransaction->registerOnCommit(fn () => $this->onDatabaseCommit());
+        $this->databaseTransaction->registerOnRollback(fn () => $this->onDatabaseRollback());
+    }
+
+    public function emit(...$args): EmitterInterface
+    {
+        if ($this->databaseTransaction->isInTransaction()) {
+            $this->delayedMessages[] = $args;
+            return $this;
+        }
+
+        return parent::emit(...$args);
+    }
+
+    private function onDatabaseCommit(): void
+    {
+        foreach ($this->delayedMessages as $args) {
+            parent::emit(...$args);
+        }
+
+        $this->delayedMessages = [];
+    }
+
+    private function onDatabaseRollback(): void
+    {
+        $this->delayedMessages = [];
+    }
+}
