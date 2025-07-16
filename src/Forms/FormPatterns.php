47eb46504c9ef73c8079931300f71a2df1ade2dc
Lubos Michalik <lubos.michalik@projektn.sk>
2025-07-16 12:59:25 +0200

    Fix the pattern of street name form check
    Add support for letters with diacritics as same regexp is used also for
    javascript validation that doesn't match previous version.
    
    Fix regexp for street number which produce errors in browser console.
    
    remp/helpdesk#3667

--- src/Forms/FormPatterns.php
+++ src/Forms/FormPatterns.php
@@ -4,8 +4,8 @@ namespace Crm\ApplicationModule\Forms;
 
 class FormPatterns
 {
-    public const STREET_NAME = '(?=[A-z0-9])[A-z0-9\s\-\.\p{L}]*';
-    public const STREET_NUMBER = '(?=[0-9])[0-9A-z\-/]*';
+    public const STREET_NAME = '(?=[\p{L}0-9])[\p{L}0-9\s\-\.\p{L}]*';
+    public const STREET_NUMBER = '(?=[0-9])[0-9A-z\-\/]*';
     public const ZIP_CODE = '(?=[0-9A-z])[0-9A-z\-\s]*';
     public const PHONE_NUMBER = '(?=[0-9])[0-9\+\s]*';
     public const PHONE_NUMBER_INTERNATIONAL = '^[+][-\s\.\/\d]{12,}$';
