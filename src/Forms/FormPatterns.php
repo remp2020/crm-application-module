0f5089be8d9db28a0d9369be9bbc2a56d923f632
Peter Dulacka <dulacka@gmail.com>
2025-11-18 14:19:43 +0100

    Fix international phone number pattern
    - Include parentheses as a valid part of
    the phone number (used for example in the US
    numbers).
    - Lower the number of required digits to 11
    to match Dutch numbers (+43123456789).
    
    remp/helpdesk#4150

--- src/Forms/FormPatterns.php
+++ src/Forms/FormPatterns.php
@@ -8,6 +8,6 @@ class FormPatterns
     public const STREET_NUMBER = '(?=[0-9])[0-9A-z\-\/]*';
     public const ZIP_CODE = '(?=[0-9A-z])[0-9A-z\-\s]*';
     public const PHONE_NUMBER = '(?=[0-9])[0-9\+\s]*';
-    public const PHONE_NUMBER_INTERNATIONAL = '^[+][-\s\.\/\d]{12,}$';
+    public const PHONE_NUMBER_INTERNATIONAL = '^[+][-\s\.\/()]*(\d[-\s\.\/()]*){11,}$';
     public const PHONE_NUMBER_FLEXIBLE = '^(' . self::PHONE_NUMBER . '|' . self::PHONE_NUMBER_INTERNATIONAL . ')$';
 }
